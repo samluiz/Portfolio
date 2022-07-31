195d1f25d7853adc2d08a3a0562fc57ba0839b67
samluiz <samuelluizrl@outlook.com>
2022-07-30 21:55:12 -0300

    Initializing repo

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Box from './Box'
 import { Container } from './styles'
+import ReactImg from '/public/img/Container1/react.svg'
 
 const Home: React.FC = () => {
     return (
         <>
             <Container>
-                <img
-                    src="/public/img/Container1/react.svg"
-                    alt="React"
-                    className="react"
-                />
+                <img src={ReactImg} alt="React" className="react" />
                 <Box />
             </Container>
         </>
