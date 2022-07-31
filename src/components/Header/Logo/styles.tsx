ec637694c27fbc672aaea96d2374835392d83599
samluiz <samuelluizrl@outlook.com>
2022-07-30 22:17:32 -0300

    First container done

--- src/components/Header/Logo/styles.tsx
+++ src/components/Header/Logo/styles.tsx
@@ -8,17 +8,46 @@ export const Container = styled.div`
     gap: 1rem;
 
     svg {
-        transition-duration: 0.2s;
+        transition-duration: 0.5s;
         &:hover {
-            a {
-            }
             transform: rotate(-90deg);
         }
     }
 
-    a {
+    span {
+        display: inline-block;
+        vertical-align: middle;
+        -webkit-transform: perspective(1px) translateZ(0);
+        transform: perspective(1px) translateZ(0);
+        box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+        position: relative;
+        overflow: hidden;
+    }
+
+    span {
+        &:before {
+            content: '';
+            position: absolute;
+            z-index: 3;
+            left: 51%;
+            right: 51%;
+            top: 0;
+            background: #cc00ff;
+            height: 1px;
+            -webkit-transition-property: left, right;
+            transition-property: left, right;
+            -webkit-transition-duration: 0.3s;
+            transition-duration: 0.3s;
+            -webkit-transition-timing-function: ease-out;
+            transition-timing-function: ease-out;
+        }
+    }
+    span {
         &:hover {
-            text-decoration: overline #cc00ff 1px;
+            &:before {
+                left: 0;
+                right: 0;
+            }
         }
     }
 `
--- src/components/Header/Navbar/styles.tsx
+++ src/components/Header/Navbar/styles.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const Container = styled.nav`
     display: grid;
     place-items: center;
-    width: fit-content;
     -webkit-text-stroke: 0.3px black;
     ul {
         display: grid;
@@ -19,9 +18,39 @@ export const Container = styled.nav`
     }
 
     a {
-        transition-duration: 0.2s;
+        display: inline-block;
+        vertical-align: middle;
+        -webkit-transform: perspective(1px) translateZ(0);
+        transform: perspective(1px) translateZ(0);
+        box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+        position: relative;
+        overflow: hidden;
+    }
+
+    a {
+        &:before {
+            content: '';
+            position: absolute;
+            z-index: 3;
+            left: 51%;
+            right: 51%;
+            top: 0;
+            background: #cc00ff;
+            height: 1px;
+            -webkit-transition-property: left, right;
+            transition-property: left, right;
+            -webkit-transition-duration: 0.3s;
+            transition-duration: 0.3s;
+            -webkit-transition-timing-function: ease-out;
+            transition-timing-function: ease-out;
+        }
+    }
+    a {
         &:hover {
-            color: gray;
+            &:before {
+                left: 0;
+                right: 0;
+            }
         }
     }
 `
